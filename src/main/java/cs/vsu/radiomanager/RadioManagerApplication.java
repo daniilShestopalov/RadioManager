79373e4d34ba4ab4e89a141fc7b44d4bf0d8836d
daniilShestopalov <danya.shestopalov@yandex.ru>
2024-05-21 13:06:12 +0300

    Init commit

---
+++ src/main/java/cs/vsu/radiomanager/RadioManagerApplication.java
@@ -0,0 +1,13 @@
+package cs.vsu.radiomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadioManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RadioManagerApplication.class, args);
+    }
+
+}
