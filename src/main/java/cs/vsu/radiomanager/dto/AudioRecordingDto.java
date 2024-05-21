9a903adacbcd71bc7e17ad0454ea7003b532ee40
daniilShestopalov <danya.shestopalov@yandex.ru>
2024-05-21 16:20:32 +0300

    Cleaned import

--- src/main/java/cs/vsu/radiomanager/dto/AudioRecordingDto.java
+++ src/main/java/cs/vsu/radiomanager/dto/AudioRecordingDto.java
@@ -1,15 +1,9 @@
 package cs.vsu.radiomanager.dto;
 
-import cs.vsu.radiomanager.model.User;
 import cs.vsu.radiomanager.model.enumerate.Status;
-import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.JdbcType;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-import org.hibernate.dialect.PostgreSQLEnumJdbcType;
 
 @Data
 @NoArgsConstructor
