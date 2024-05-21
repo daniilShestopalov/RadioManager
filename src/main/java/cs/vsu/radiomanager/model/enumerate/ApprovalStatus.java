1dcef52da265a8affc75d30537f488e75b9a9008
daniilShestopalov <danya.shestopalov@yandex.ru>
2024-05-21 16:26:31 +0300

    Renamed Status to ApprovalStatus

---
+++ src/main/java/cs/vsu/radiomanager/model/enumerate/ApprovalStatus.java
@@ -0,0 +1,7 @@
+package cs.vsu.radiomanager.model.enumerate;
+
+public enum ApprovalStatus {
+    APPROVED,   // проверено
+    PENDING,    // на рассмотрении
+    REJECTED    // не прошло проверку
+}
