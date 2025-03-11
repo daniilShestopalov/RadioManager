6217c0039c9126cf924afd0259f7acf1dcb353bb
daniilShestopalov <113010840+daniilShestopalov@users.noreply.github.com>
2025-03-11 17:50:58 +0300

    Updated CityRep.java

--- src/main/java/cs/vsu/radiomanager/repository/CityRep.java
+++ src/main/java/cs/vsu/radiomanager/repository/CityRep.java
@@ -1,13 +1,14 @@
 package cs.vsu.radiomanager.repository;
 
 import cs.vsu.radiomanager.model.City;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface CityRep {
+public interface CityRep extends JpaRepository<City, Integer> {
 
     Optional<City> findCityById(Long id);
     Optional<City> findCityByNameAndRegion(String name, String region);
