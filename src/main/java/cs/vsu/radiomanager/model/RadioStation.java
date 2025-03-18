6545c67b9c0d5a708cd9a9b8771b333c9ccba5de
daniilShestopalov <113010840+daniilShestopalov@users.noreply.github.com>
2025-03-18 14:14:33 +0300

    Fix model RadioStation

--- src/main/java/cs/vsu/radiomanager/model/RadioStation.java
+++ src/main/java/cs/vsu/radiomanager/model/RadioStation.java
@@ -2,6 +2,7 @@ package cs.vsu.radiomanager.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.OnDelete;
@@ -19,10 +20,11 @@ public class RadioStation {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", unique = true, length = 100, nullable = false)
+    @Column(name = "name", unique = true, nullable = false)
     private String name;
 
     @Digits(integer = 3, fraction = 1)
+    @Positive
     @Column(name = "frequency", nullable = false, precision = 4, scale = 1)
     private BigDecimal frequency;
 
