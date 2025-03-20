6ac6c7a3fd82dc4ff7a7c35e9e1e8476a5dcff80
daniilShestopalov <113010840+daniilShestopalov@users.noreply.github.com>
2025-03-20 14:58:47 +0300

    Added search by StartTime, EndTime and RadioStation

--- src/main/java/cs/vsu/radiomanager/repository/BroadcastSlotRep.java
+++ src/main/java/cs/vsu/radiomanager/repository/BroadcastSlotRep.java
@@ -1,6 +1,7 @@
 package cs.vsu.radiomanager.repository;
 
 import cs.vsu.radiomanager.model.BroadcastSlot;
+import cs.vsu.radiomanager.model.RadioStation;
 import cs.vsu.radiomanager.model.enumerate.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -13,6 +14,7 @@ import java.util.Optional;
 public interface BroadcastSlotRep extends JpaRepository<BroadcastSlot, Integer> {
 
     Optional<BroadcastSlot> findById(Long id);
+    Optional<BroadcastSlot> findByStartTimeAndEndTimeAndRadioStation(LocalDateTime startTime, LocalDateTime endTime, RadioStation radioStation);
     List<BroadcastSlot> findByStatus(Status status);
     Optional<BroadcastSlot> findByStartTimeAndEndTime(LocalDateTime startTime, LocalDateTime endTime);
     List<BroadcastSlot> findByStartTimeBetween(LocalDateTime start, LocalDateTime end);
