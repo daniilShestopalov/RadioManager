c0d4ae42137911b08f27fa3ae4f711e6a86ce188
daniilShestopalov <113010840+daniilShestopalov@users.noreply.github.com>
2025-03-20 15:21:43 +0300

    Fix

--- src/main/java/cs/vsu/radiomanager/repository/BroadcastSlotRep.java
+++ src/main/java/cs/vsu/radiomanager/repository/BroadcastSlotRep.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 public interface BroadcastSlotRep extends JpaRepository<BroadcastSlot, Integer> {
 
     Optional<BroadcastSlot> findById(Long id);
-    Optional<BroadcastSlot> findByStartTimeAndEndTimeAndRadioStation(LocalDateTime startTime, LocalDateTime endTime, RadioStation radioStation);
+    Optional<BroadcastSlot> findByStartTimeAndEndTimeAndRadioStationId(LocalDateTime startTime, LocalDateTime endTime, Long radioStationId);
     List<BroadcastSlot> findByStatus(Status status);
     Optional<BroadcastSlot> findByStartTimeAndEndTime(LocalDateTime startTime, LocalDateTime endTime);
     List<BroadcastSlot> findByStartTimeBetween(LocalDateTime start, LocalDateTime end);
