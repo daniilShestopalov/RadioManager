e0405ab58e4aafe2b5e0ea59dc69577c95426ff0
daniilShestopalov <113010840+daniilShestopalov@users.noreply.github.com>
2025-04-03 15:22:01 +0300

    Added config for create init admin

--- src/main/java/cs/vsu/radiomanager/service/AuthService.java
+++ src/main/java/cs/vsu/radiomanager/service/AuthService.java
@@ -58,6 +58,7 @@ public class AuthService {
         return exists;
     }
 
+    // TODO Add code for change password
     public boolean updatePasswordByLogin(@NotNull String login, @NotNull String password) {
         LOGGER.info("Updating password by login: {}", login);
         Optional<User> userOptional = userRepository.findByLogin(login);
