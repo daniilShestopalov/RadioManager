4c88918b842f3ad12f29481715e08d93dd1ef7b5
daniilShestopalov <113010840+daniilShestopalov@users.noreply.github.com>
2025-04-10 17:00:19 +0300

    Created SystemEntityConfig

---
+++ src/main/java/cs/vsu/radiomanager/config/SystemEntityConfig.java
@@ -0,0 +1,52 @@
+package cs.vsu.radiomanager.config;
+
+import cs.vsu.radiomanager.dto.UserDto;
+import cs.vsu.radiomanager.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class SystemEntityConfig {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(SystemEntityConfig.class);
+
+    @Value("${system.entity.login}")
+    private String entityLogin;
+
+    @Value("${system.entity.password}")
+    private String entityPassword;
+
+    @Value("${system.entity.name}")
+    private String entityName;
+
+    @Value("${system.entity.surname}")
+    private String entitySurname;
+
+    @Value("${system.entity.balance}")
+    private Double entityBalance;
+
+    @Bean
+    public CommandLineRunner createSystemEntity(AuthService authService) {
+        return args -> {
+            if (!authService.checkEmailExists(entityLogin)) {
+                UserDto systemEntity = new UserDto();
+                systemEntity.setLogin(entityLogin);
+                systemEntity.setPassword(entityPassword);
+                systemEntity.setName(entityName);
+                systemEntity.setSurname(entitySurname);
+                systemEntity.setBalance(entityBalance);
+                authService.registerUser(systemEntity);
+                LOGGER.info("System entity user created successfully.");
+            } else {
+                LOGGER.info("System entity user already exists.");
+            }
+        };
+    }
+
+}
