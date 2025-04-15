426db6f13739aaa2a7204b45726ecb6c5344650b
daniilShestopalov <113010840+daniilShestopalov@users.noreply.github.com>
2025-04-15 21:06:06 +0300

    Created 3 new methods

--- src/main/java/cs/vsu/radiomanager/repository/BroadcastSlotRep.java
+++ src/main/java/cs/vsu/radiomanager/repository/BroadcastSlotRep.java
@@ -18,6 +18,8 @@ public interface BroadcastSlotRep extends JpaRepository<BroadcastSlot, Integer> 
     List<BroadcastSlot> findByStatus(Status status);
     Optional<BroadcastSlot> findByStartTimeAndEndTime(LocalDateTime startTime, LocalDateTime endTime);
     List<BroadcastSlot> findByStartTimeBetween(LocalDateTime start, LocalDateTime end);
-
+    List<BroadcastSlot> findByRadioStationIdAndStartTimeAfter(Long radioStationId, LocalDateTime startTime);
+    List<BroadcastSlot> findByRadioStationIdAndStatusAndStartTimeAfter(Long radioStationId, Status status,LocalDateTime startTime);
+    boolean deleteAllByRadioStationIdAndStartTimeAfter(Long radioStationId, LocalDateTime startTime);
 
 }
