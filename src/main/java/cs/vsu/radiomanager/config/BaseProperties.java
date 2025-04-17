31176ec931171d5339eecd509c14e49be64c93ac
daniilShestopalov <113010840+daniilShestopalov@users.noreply.github.com>
2025-04-17 18:51:17 +0300

    Creates base properties for program work

---
+++ src/main/java/cs/vsu/radiomanager/config/BaseProperties.java
@@ -0,0 +1,27 @@
+package cs.vsu.radiomanager.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalTime;
+import java.util.List;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "base")
+public class BaseProperties {
+
+    @Data
+    public static class TimeWindow {
+        private LocalTime start;
+        private LocalTime end;
+    }
+
+    private Double filePrice;
+
+    private Double priorityMultiplier;
+
+    private List<TimeWindow> priority1;
+
+}
