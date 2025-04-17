635ac263149dc0f9179a7afd7601a2e40142a294
daniilShestopalov <113010840+daniilShestopalov@users.noreply.github.com>
2025-04-17 17:03:55 +0300

    Added cut-off of old slot

--- src/main/java/cs/vsu/radiomanager/controller/api/BroadcastSlotController.java
+++ src/main/java/cs/vsu/radiomanager/controller/api/BroadcastSlotController.java
@@ -19,7 +19,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/broadcast-slot")
